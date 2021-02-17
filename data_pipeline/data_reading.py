1f2fed59c4019b41d6a853b37206d3df9b4b4d10
ranapop <roxanappop7@gmail.com>
2021-02-17 17:12:17 +0200

    amr reading: put pattern in constant and add comment about pattern

--- data_pipeline/data_reading.py
+++ data_pipeline/data_reading.py
@@ -3,6 +3,18 @@ import re
 import os
 import definitions
 
+"""Pattern for extracting the id, tokens, alignments and amr from an entry string.
+An entry example follows:
+# ::id bolt12_07_4800.1 ::amr-annotator SDL-AMR-09 ::preferred
+# ::tok Establishing Models in Industrial Innovation
+# ::alignments 0-1 1-1.1 2-1.1.1.r 3-1.1.1.1 4-1.1.1
+(e / establish-01~e.0 
+      :ARG1 (m / model~e.1 
+            :mod~e.2 (i / innovate-01~e.4 
+                  :ARG1 (i2 / industry~e.3))))
+"""
+AMR_PATTERN = r'# ::id(.*)\n# ::tok(.*)\n# ::alignments(.*)\n(?=\()(.*)'
+
 def extract_triples(path: str):
   """
   Return a list of (id, tokens, amr string) triples for an AMR data file.
@@ -16,8 +28,7 @@ def extract_triples(path: str):
   triples = []
   for data_block in data_blocks:
     # Use regex to extract the needed info from a text block.
-    pattern = r'# ::id(.*)\n# ::tok(.*)\n# ::alignments(.*)\n(?=\()(.*)'
-    match = re.search(pattern, data_block, re.DOTALL)
+    match = re.search(AMR_PATTERN, data_block, re.DOTALL)
     if match:
       id, sentence, alignments, amr = match.groups()
       triples.append((id, sentence, amr))
